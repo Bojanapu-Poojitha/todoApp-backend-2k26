4dfc5e7ed4f3ec95c7044134b7407ce1deabb535
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-01-02 19:45:56 +0530

    Create a config folder with generate key files

---
+++ src/Config/ToDoConfig.ts
@@ -0,0 +1,23 @@
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const servicePath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
+
+if (!servicePath) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT_PATH is not set");
+}
+
+const serviceAccount = JSON.parse(
+  fs.readFileSync(servicePath, "utf-8")
+);
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+export const db = getFirestore();
+export const tasksCollection = db.collection("toDoTasks");
