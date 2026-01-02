515b4fcfc29d99f7f25b206aa29cfe518823b036
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-01-02 17:48:54 +0530

    Create a src folder with basic create App file

---
+++ src/App.ts
@@ -0,0 +1,10 @@
+import express, { Request } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`server is running at ${PORT}`);
+});
