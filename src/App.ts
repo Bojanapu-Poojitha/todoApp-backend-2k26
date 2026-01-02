02ad4b3255eb62b5cc0f5fa8342363be0eae35f1
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-01-02 18:45:34 +0530

    Call the route path in main file

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,10 @@
 import express, { Request } from "express";
-
+import router from "./routes/TaskRouter";
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
+app.use('/tasks', router);
 app.listen(PORT, () => {
   console.log(`server is running at ${PORT}`);
 });
