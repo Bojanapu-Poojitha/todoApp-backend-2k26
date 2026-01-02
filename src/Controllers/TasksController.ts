7053aaef592ab0207857fc00fe4b4a33aff6c699
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-01-02 19:48:01 +0530

    Call the add service and modified it in controllers folder

--- src/Controllers/TasksController.ts
+++ src/Controllers/TasksController.ts
@@ -1,23 +1,18 @@
 import { Request, Response } from "express";
 import { Task } from "../type/Tasks";
+import { addNewItem } from "../Services/TaskServices";
 const tasks: Task[] = [];
 export const addItem = async (req: Request, res: Response) => {
-  const newItem: Task = req.body;
+  try {
+    const newTask: Task = req.body;
+    const task = await addNewItem(newTask);
 
-  if (!newItem.title) {
-    return res.status(400).json({ message: "task title is mandatory" });
+    res.status(201).json({ message: "new item created", data: task });
+  } catch (e) {
+    res.status(500).json({ message: "failing" });
   }
-  const task: Task = {
-    id: "",
-    title: newItem.title,
-    description: newItem.description,
-    status: newItem.status || "pending",
-    priority: newItem.priority || "medium",
-    deadline: newItem.deadline,
-  };
-  tasks.push(task);
-  return res.status(201).json({ message: "item added successfully", task });
 };
 export const getItems = async (req: Request, res: Response) => {
   return res.status(200).json({ tasks });
 };
+
