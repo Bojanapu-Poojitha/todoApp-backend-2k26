8bd83c7ffb3d059a0650dfadc37031112c2fd003
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-01-04 19:31:45 +0530

    Mock the service folder files

--- tests/TaskController.test.ts
+++ tests/TaskController.test.ts
@@ -1,3 +1,8 @@
 import { Request,Response } from "express";
 import { addItem,getTask,deleteTask,update } from "../src/Controllers/TasksController";
 import * as TaskServices from '../src/Services/TaskServices';
+
+jest.mock('../src/Services/TaskServices');
+describe('the taskController is ',()=>{
+    
+})
