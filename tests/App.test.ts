5edcd97d8a7a3063f521da66dec6f45d9fcb468d
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-01-05 11:54:46 +0530

    Implement a to get all items test case

--- tests/App.test.ts
+++ tests/App.test.ts
@@ -1,9 +1,11 @@
 import request from 'supertest';
 import {app} from '../src/App';
-import { addNewItem } from '../src/Services/TaskServices';
+import { addNewItem, getItems } from '../src/Services/TaskServices';
+
 
 jest.mock('../src/Services/TaskServices',()=>({
-    addNewItem: jest.fn()
+    addNewItem: jest.fn(),
+    getItems:jest.fn()
 }));
 
 describe('the testing for controller and services', ()=>{
@@ -22,5 +24,32 @@ describe('the testing for controller and services', ()=>{
         expect(response.body.message).toBe('new item created');
         expect(response.body.data).toEqual(task);
         
+    });
+
+    test('/get method, to display all items',async()=>{
+        const tasks =[
+            {
+                id:'1',
+                title:'poojitha',
+                description:'this is my name',
+                status:'in progress',
+                priority:'high',
+                deadline :'2026-01-03'
+            },
+            {
+                id:'4',
+                title:'kalyan',
+                description:'my bro name',
+                status:'pending',
+                priority:'medium',
+                deadline:'2026-01-05'
+            },
+
+        ];
+        (getItems as jest.Mock).mockResolvedValue(tasks);
+        const response = await request(app).get('/tasks');
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('Items');
+        expect(response.body.data).toEqual(tasks);
     })
 })
