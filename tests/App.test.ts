d449a68f1bae995f184ffa50701bb941d0e25f93
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-01-05 11:44:48 +0530

    Implement a add new task test case and mock the add new item service

--- tests/App.test.ts
+++ tests/App.test.ts
@@ -7,5 +7,20 @@ jest.mock('../src/Services/TaskServices',()=>({
 }));
 
 describe('the testing for controller and services', ()=>{
-    
+    test('/post method, to add a new task ', async()=>{
+        const task = {
+            title:'poojit',
+            description:'this is my name',
+            status:'in progress',
+            priority:'medium',
+            deadline:'2026-01-05'
+        };
+        (addNewItem as jest.Mock).mockResolvedValue(task);
+
+        const response = await request(app).post('/tasks').send(task);
+        expect(response.status).toBe(201);
+        expect(response.body.message).toBe('new item created');
+        expect(response.body.data).toEqual(task);
+        
+    })
 })
