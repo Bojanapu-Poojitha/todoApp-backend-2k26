4c440fa000b4b0681a2d12190b1edbdb38dfc0d0
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-01-07 16:06:38 +0530

    Mock the todo config file and add describe

--- tests/TaskServices.test.ts
+++ tests/TaskServices.test.ts
@@ -1,3 +1,8 @@
 import { addNewItem ,deleteItem,getItems,updateTask} from "../src/Services/TaskServices";
 import { tasksCollection } from "../src/Config/ToDoConfig";
 import { Task } from "../src/type/Tasks";
+
+jest.mock("../src/Config/ToDoConfig");
+describe('service file test cases',()=>{
+  
+})
