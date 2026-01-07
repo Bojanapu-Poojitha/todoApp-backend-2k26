db2bea482be6a1552224e726da4156836e68dc3f
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-01-07 16:05:29 +0530

    Create a task service test file with import paths

---
+++ tests/TaskServices.test.ts
@@ -0,0 +1,3 @@
+import { addNewItem ,deleteItem,getItems,updateTask} from "../src/Services/TaskServices";
+import { tasksCollection } from "../src/Config/ToDoConfig";
+import { Task } from "../src/type/Tasks";
