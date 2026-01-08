80a39bc1e756b57b8381a4debfa1b90f8235044d
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-01-08 12:07:49 +0530

    Removed unused variable and set correctly import paths in controllers

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
-import { Task } from "../type/Tasks";
-import { addNewItem,getItems, deleteItem, updateTask } from "../Services/TaskServices";
-import { tasksCollection } from "../Config/ToDoConfig";
-const tasks: Task[] = [];
+import { Task } from "../type/tasks";
+import {
+  addNewItem,
+  getItems,
+  deleteItem,
+  updateTask,
+} from "../services/taskServices";
+import { tasksCollection } from "../config/toDoConfig";
+
 export const addItem = async (req: Request, res: Response) => {
   try {
     const newTask: Task = req.body;
@@ -22,33 +27,29 @@ export const getTask = async (req: Request, res: Response) => {
     res.status(500).json({ message: "failed to get items" });
   }
 };
-export const deleteTask = async(req:Request,res:Response)=>{
-  try{
-  const {id} = req.params;
-   console.log(`deleting task with ID: ${id}`);
+export const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    console.log(`deleting task with ID: ${id}`);
     const deleteId = tasksCollection.doc(id);
-      const existsId = await deleteId.get();
-       if (!existsId.exists) {
-      throw new Error('this id task not found');
+    const existsId = await deleteId.get();
+    if (!existsId.exists) {
+      throw new Error("this id task not found");
     }
-   await deleteItem(id);
-   res.status(200).json({message:"item is deleted successfully"});
+    await deleteItem(id);
+    res.status(200).json({ message: "item is deleted successfully" });
+  } catch (e) {
+    res.status(500).json({ message: "something went wrong" });
   }
-  catch(e){
-   res.status(500).json({message:'something went wrong'});
-  }
+};
 
-
-}
-
-export const update = async(req:Request,res:Response)=>{
-  try{
+export const update = async (req: Request, res: Response) => {
+  try {
     const id = req.params.id;
-    const updatedTask: Partial<Task>= req.body;
-    const newUpdate = await updateTask(id,updatedTask);
-    res.status(200).json({message:"task is updated perfectly"})
+    const updatedTask: Partial<Task> = req.body;
+    const newUpdate = await updateTask(id, updatedTask);
+    res.status(200).json({ message: "task is updated perfectly" });
+  } catch (e) {
+    res.status(500).json({ message: "something went wrong while updating" });
   }
-  catch(e){
-    res.status(500).json({message:"something went wrong while updating"});
-  }
-}
+};
