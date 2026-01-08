4081bfc54ff31dc77e2824baddea5a10d0e526e9
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-01-08 12:12:58 +0530

    Remove hard code value and dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Request } from "express";
 import router from "./routes/TaskRouter";
 import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config();
 export const app = express();
 app.use(cors());
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use('/tasks', router);
